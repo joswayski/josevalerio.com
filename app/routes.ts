dcddf73b3f31c8c419b070b448baf0b64b832847
Jose Valerio <contact@josevalerio.com>
2025-09-23 04:08:57 -0400

    saving

--- app/routes.ts
+++ app/routes.ts
@@ -1,9 +1,15 @@
-import { type RouteConfig, index, route, layout } from "@react-router/dev/routes";
+import {
+  type RouteConfig,
+  index,
+  route,
+  layout,
+} from "@react-router/dev/routes";
 
 export default [
   index("routes/home.tsx"),
   layout("layouts/blog-layout.tsx", [
     route("/just-do-the-thing", "routes/just-do-the-thing.tsx"),
     route("/rust-json-logging", "routes/rust-json-logging.tsx"),
+    route("/homelab", "routes/homelab.tsx"),
   ]),
 ] satisfies RouteConfig;
